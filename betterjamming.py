b1bf4f5a044e73c5e13dbd96e65434f4349a67e6
Satcom <47340064+hurikayne@users.noreply.github.com>
2020-03-18 18:05:52 +0100

    update "reslove wifi problemes"

--- betterjamming.py
+++ betterjamming.py
@@ -25,7 +25,8 @@ def main():
     while answ:
         print("""
          1.Jam !
-         2.CREDITS
+         2.Resolve wifi problems
+         3.CREDITS
 
          00.Exit
          """)
@@ -34,9 +35,7 @@ def main():
             template()
             print " Lets Jam"
             print "-" * 64
-            for ifaceName in interfaces():
-                addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP address'}] )]
-                print '   		%s: %s' % (ifaceName, ', '.join(addresses))
+            template()
             interface = raw_input('which interface you want use ?  ')
             print "Remember to copy your mac address and channel victim !"
             time.sleep(2)
@@ -46,10 +45,20 @@ def main():
             print "Put interface in monitor mode..."
             os.system("xterm -e 'airmon-ng start "+interface+"' '"+channel+"' && read'")
             os.system("xterm -e 'aireplay-ng -o 999 -c "+interface+" && read'")
+            time.sleep(2)
+            main()
         elif ans=="2":
+            template()
+            print "Reset Interface"
+            interface = raw_input("Which interface you want to reset ?  ")
+            os.system("xterm -e 'iwconfig "+interface+" down && iwconfig '"+interface+"' up'")
+            print "Interface "+interface+" has been reset. "
+            time.sleep(2)
+            main()
+
+        elif ans=="3":
             credits()
         elif ans=="00":
-          my_quit_fn()
           ans = None
         else:
             os.system('clear')
